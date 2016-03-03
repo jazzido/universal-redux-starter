ded5c2b4201f454c594432537a13d232a8fa8939
Manuel Aristaran <jazzido@jazzido.com>
2016-03-03 18:19:23 -0500

    reproduce update-location bug

--- src/containers/App/App.js
+++ src/containers/App/App.js
@@ -1,7 +1,20 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
 
+
+@connect(
+    (state, ownProps) => ({
+        clicks: state.button.clicks
+    })
+)
 export default class App extends Component {
   render() {
-    return (<div>Hello Universal Redux!</div>);
+      return (
+          <div>
+              Hello Universal Redux!.
+              <button onClick={() => this.props.dispatch({type: 'BUTTON_CLICK'})}>Click me</button>
+              Clicks: <span>{this.props.clicks}</span>
+          </div>
+      );
   }
 }
--- src/redux/modules/index.js
+++ src/redux/modules/index.js
@@ -3,6 +3,17 @@ import { routeReducer } from 'react-router-redux';
 import { reducer as reduxAsyncConnect } from 'redux-async-connect';
 
 export default combineReducers({
-  reduxAsyncConnect,
-  routing: routeReducer
+    reduxAsyncConnect,
+    routing: routeReducer,
+    button: function(state={ clicks: 0 }, action={}) {
+        switch(action.type) {
+            case 'BUTTON_CLICK':
+            return {
+                ...state,
+                clicks: state.clicks + 1
+            };
+            default:
+                return state;
+        }
+    }
 });
